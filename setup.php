66c4180605060038ad5ab164ef0796c3c755ccb5
Márcio Fernandes <marcio.fernandes@verdanatech.com>
2024-05-07 10:08:58 -0300

    Add new version

--- setup.php
+++ setup.php
@@ -1,6 +1,36 @@
 <?php
+/*
+-------------------------------------------------------------------------
+MailAnalyzer plugin for GLPI
+Copyright (C) 2011-2024 by Raynet SAS a company of A.Raymond Network.
 
-define ("PLUGIN_MAILANALYZER_VERSION", "3.0.0");
+https://www.araymond.com/
+-------------------------------------------------------------------------
+
+LICENSE
+
+This file is part of MailAnalyzer plugin for GLPI.
+
+This file is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this plugin. If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------
+ */
+
+define ("PLUGIN_MAILANALYZER_VERSION", "3.2.0");
+// Minimal GLPI version, inclusive
+define('PLUGIN_MAILANALYZER_MIN_GLPI', '10.0');
+// Maximum GLPI version, exclusive
+define('PLUGIN_MAILANALYZER_MAX_GLPI', '10.1');
 
 /**
  * Summary of plugin_init_mailanalyzer
@@ -26,6 +56,11 @@ function plugin_init_mailanalyzer() {
       'Ticket' => ['PluginMailAnalyzer', 'plugin_item_purge_mailanalyzer']
    ];
 
+    if (Session::haveRightsOr("config", [READ, UPDATE])) {
+        Plugin::registerClass('PluginMailanalyzerConfig', ['addtabon' => 'Config']);
+        $PLUGIN_HOOKS['config_page']['mailanalyzer'] = 'front/config.form.php';
+    }
+
 }
 
 
@@ -43,8 +78,8 @@ function plugin_version_mailanalyzer() {
       'homepage'     => 'https://github.com/tomolimo/mailanalyzer',
       'requirements' => [
          'glpi' => [
-            'min' => '10.0',
-            'max' => '10.1'
+            'min' => PLUGIN_MAILANALYZER_MIN_GLPI,
+            'max' => PLUGIN_MAILANALYZER_MAX_GLPI
             ]
          ]
    ];
@@ -57,9 +92,9 @@ function plugin_version_mailanalyzer() {
  * @return bool
  */
 function plugin_mailanalyzer_check_prerequisites() {
-   if (version_compare(GLPI_VERSION, '10.0', 'lt')
-       && version_compare(GLPI_VERSION, '10.1', 'ge')) {
-      echo "This plugin requires GLPI >= 10.0 and < 10.1";
+   if (version_compare(GLPI_VERSION, PLUGIN_MAILANALYZER_MIN_GLPI, 'lt')
+       && version_compare(GLPI_VERSION, PLUGIN_MAILANALYZER_MAX_GLPI, 'ge')) {
+      echo "This plugin requires GLPI >= " . PLUGIN_MAILANALYZER_MIN_GLPI ." and < " . PLUGIN_MAILANALYZER_MAX_GLPI;
       return false;
    } else {
       if (!class_exists('mailanalyzer_check_prerequisites')) {
